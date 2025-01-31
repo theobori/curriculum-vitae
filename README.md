f34968b2258aa9b2e08b6a209f1711e43c05e52b
Théo Bori <theo1.bori@epitech.eu>
2025-01-31 03:22:59 +0100

    :sparkles: Mirror the private repository

---
+++ README.md
@@ -0,0 +1,11 @@
+# Curriculum vitae
+
+[![build](https://github.com/theobori/curriculum-vitae/actions/workflows/build.yml/badge.svg)](https://github.com/theobori/curriculum-vitae/actions/workflows/build.yml)
+
+[![built with nix](https://builtwithnix.org/badge.svg)](https://builtwithnix.org)
+
+This is a public version of my lightweight and minimalist curriculum vitae following the KISS principle.
+It does not contains any sensible informations (phone number, etc.)
+
+To view the compiled PDFs, you can check the [pdf](https://github.com/theobori/curriculum-vitae/tree/pdf) branch.
+
---
+++ default.nix
@@ -0,0 +1,57 @@
+{
+  lib,
+  stdenvNoCC,
+  texlive,
+}:
+let
+  inherit (lib.strings) intersperse concatStrings;
+
+  langs' = [
+    "fr"
+    "en"
+  ];
+
+  langs = concatStrings (intersperse " " langs');
+in
+stdenvNoCC.mkDerivation {
+  pname = "curriculum-vitae";
+  version = "0.0.1";
+
+  src = ./.;
+
+  buildInputs = [
+    (texlive.combine {
+      inherit (texlive)
+        scheme-medium
+        fourier
+        lastpage
+        etaremune
+        datetime2
+        ;
+    })
+  ];
+
+  buildPhase = ''
+    runHook preBuild
+
+    for lang in ${langs}; do
+      cd $lang
+      pdflatex theobori_cv_$lang.tex
+      cd -
+    done
+
+    runHook postBuild
+  '';
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out
+
+    for lang in ${langs}; do
+      mv $lang/theobori_cv_$lang.pdf $out
+    done
+
+    runHook postInstall
+  '';
+}
